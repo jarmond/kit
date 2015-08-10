e370e381a532a2042a7767722b0858306c66049b
Jonathan Armond <jon.armond@gmail.com>
2015-08-10 10:29:28 +0100

    Add option to export movie

--- kitPlayMovie.m
+++ kitPlayMovie.m
@@ -1,16 +1,36 @@
-function kitPlayMovie(filename)
+function kitPlayMovie(filename,outfile,docrop)
 % KITPLAYMOVIE Play movie file
 
-if nargin<1
+if nargin<1 || isempty(filename)
   [filename,pathname] = uigetfile(...
     kitSupportedFormats(1),'Locate movie to play');
 else
   [pathname,filename,ext] = fileparts(filename);
   filename = [filename ext];
 end
+filename = fullfile(pathname,filename);
+
+if nargin<2
+  outfile=[];
+end
+
+if nargin>2 && docrop == 1
+  [crop,cropSize] = kitCropMovie(filename);
+  if mod(cropSize(1),2) ~= 0
+    crop(3) = crop(3)-1;
+    cropSize(1) = cropSize(1)-1;
+  end
+  if mod(cropSize(2),2) ~= 0
+    crop(4) = crop(4)-1;
+    cropSize(2) = cropSize(2)-1;
+  end
+else
+  crop = [];
+  cropSize = [];
+end
 
 % Open movie.
-[md,reader] = kitOpenMovie(fullfile(pathname,filename));
+[md,reader] = kitOpenMovie(filename);
 opts.saturate = [1 99.9];
 if size(opts.saturate,1)<md.nChannels
   opts.saturate = repmat(opts.saturate,[md.nChannels,1]);
@@ -23,12 +43,24 @@ fprintf('dt = %.2fs  length = %.1f min\n', dt, len/60);
 
 figure;
 clf;
+if ~isempty(outfile)
+  vWriter = VideoWriter(outfile, 'MPEG-4');
+  vWriter.FrameRate = 5;
+  vWriter.Quality = 95;
+  open(vWriter);
+end
+
+if isempty(cropSize)
+  imgSize = md.frameSize(1:2)
+else
+  imgSize = cropSize(1:2)
+end
 
 for i=1:md.nFrames
-  rgbImg = zeros([md.frameSize(1:2), 3]);
+  rgbImg = zeros([imgSize, 3]);
   for c=1:min(md.nChannels, maxMergeChannels)
     % Read stack.
-    img = kitReadImageStack(reader, md, i, c, [], 0);
+    img = kitReadImageStack(reader, md, i, c, crop, 0);
 
     % Max project.
     img = max(img, [], 3);
@@ -45,4 +77,12 @@ for i=1:md.nFrames
   imshow(rgbImg);
   axis image;
   drawnow;
+  if ~isempty(outfile)
+    % Save frame.
+    writeVideo(vWriter, getframe);
+  end
+end
+
+if ~isempty(outfile)
+  close(vWriter);
 end
