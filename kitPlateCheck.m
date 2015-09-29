40ffc273039b166ebe9372b074e2467cbfec1c28
Jonathan Armond <jon.armond@gmail.com>
2015-09-29 11:24:05 +0100

    Allow multiple colors in plate check

--- kitPlateCheck.m
+++ kitPlateCheck.m
@@ -1,15 +1,18 @@
-function kitPlateCheck(job,channel)
+function kitPlateCheck(job,varargin)
 % KITPLATECHECK Visualize plate in xy-plane for coord-sys verification
 %
-%    KITPLATECHECK(JOB,CHANNEL) Visualize plate in xy-plane for
+%    KITPLATECHECK(JOB,...) Visualize plate in xy-plane for
 %    coordinate-system verification. JOB may be either a job struct, loaded via
 %    kitLoadJob, or a sisterList.
 %
-%    CHANNEL Optional, specify tracked channel to display.
+%    'channel': Optional, specify tracked channel to display.
+%
+%    'colors': Optional, set to 1 to show multiple colors to distinguish
+%    tracks. Otherwise uses colors to distinguish sisters
 
-if nargin<2
-  channel = 1;
-end
+opts.channel = 1;
+opts.colors = 0;
+opts = processOptions(opts,varargin{:});
 
 if isfield(job,'dataStruct')
   % Is a job struct.
@@ -22,7 +25,12 @@ end
 coords1 = horzcat(sisterList.coords1);
 coords2 = horzcat(sisterList.coords2);
 figure;
-plot(coords1(:,1:6:end),coords1(:,2:6:end),'b-',...
-     coords2(:,1:6:end),coords2(:,2:6:end),'g-');
+if opts.colors
+  plot(coords1(:,1:6:end),coords1(:,2:6:end),...
+       coords2(:,1:6:end),coords2(:,2:6:end));
+else
+  plot(coords1(:,1:6:end),coords1(:,2:6:end),'b-',...
+       coords2(:,1:6:end),coords2(:,2:6:end),'g-');
+end
 xlabel('x');
 ylabel('y');
