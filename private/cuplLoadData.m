f98629ce57fcaad4ac88119d0ef5f8dbcdca82bf
Jonathan Armond <jon.armond@gmail.com>
2015-10-15 10:57:19 +0100

    Check for sisterList field

--- private/cuplLoadData.m
+++ private/cuplLoadData.m
@@ -38,7 +38,7 @@ for i=1:nROIs
     continue;
   end
 
-  if ~isempty(data.sisterList) && ~isempty(data.sisterList(1).coords1)
+  if isfield(data,'sisterList') && ~isempty(data.sisterList) && ~isempty(data.sisterList(1).coords1)
     % Sister track coords, stored as sequential coordinates, e.g.
     % an.sisterCoords1(:,1:3) selects x,y,z of first sister.
     nSisters = length(data.sisterList);
