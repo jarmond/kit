e243d75a51e6e436c8ce76b568d2bf16ec35a997
Chris <chris@mechanochemistry.org>
2016-10-21 13:48:11 +0100

    Changes the chromatic shift measurement required by a given jobset, with the option of doing this for the set of job structures too. Chromatic shift can be provided as a full cell structure, or specifically for a given channel vector.

---
+++ chrsChangeChromaticShift.m
@@ -0,0 +1,93 @@
+function [jobset,jobs] = chrsChangeChromaticShift(jobset,newShift,varargin)
+%CHRSCHANGECHROMATICSHIFT Changes the chromatic shift measurements given to
+%    a certain jobset.
+%
+%    CHRSCHANGECHROMATICSHIFT(JOBSET,NEWSHIFT,...) Gives a new JOBSET, and
+%    JOBS if requested by the user, containing the chromatic shift
+%    measurements given in NEWSHIFT.
+%
+%    Options, defaults in {}:-
+%
+%    chanVect: {[1 2]} or two numbers between 1 and 3. The two channels for
+%         which the new chromatic shift measurement applies, directed from
+%         the first to the second number of the vector.
+%
+%    andJobs: {0} or 1. Whether or not to correct all jobs already
+%         processed.
+%         NB: This is not recommended for significant changes in chromatic
+%         shift measurements.
+%
+%
+% Copyright (c) 2016 C. A. Smith
+
+% default options
+opts.chanVect = [];
+opts.andJobs = 0;
+% process user-defined options
+opts = processOptions(opts,varargin{:});
+
+%% Change jobset information
+
+% check whether the chromatic shift provided is in cell form, otherwise
+% ensure a channel vector is provided
+if isempty(opts.chanVect)
+    if ~iscell(newShift)
+       error('If providing only one chromatic shift vector, need to specify channels.')
+    else
+        chrShift = newShift;
+    end
+    
+% check whether the channel vector includes only allowed channels
+elseif ~isempty(setdiff(opts.chanVect,1:3))
+    error('Can only choose channels from 1 to 3.')
+    
+else % if channel vector provided, proceed as appropriate
+
+    fromChan = opts.chanVect(1);
+    toChan = opts.chanVect(2);
+    
+    % get original chromatic shift cell array
+    chrShift = jobset.options.chrShift.result;
+    
+    % replace the newShift with only the vector specified in chanVect
+    if iscell(newShift)
+        warning('Replacing only specified channel from new shift structure.')
+        newShift = newShift{fromChan,toChan};
+    end
+    
+    % ensure that only the coordinate measurements are replaced
+    lenShift = length(newShift);
+    chrShift{fromChan,toChan}(1:lenShift) = newShift;
+    
+    % replace the mirrored measurement (ie. from toChan to fromChan)
+    revShift = newShift;
+    revShift(1:3) = revShift(1:3)*-1;
+    chrShift{toChan,fromChan}(1:lenShift) = revShift;
+    
+end
+
+% change chromatic shift information in the jobset and save
+jobset.options.chrShift.result = chrShift;
+kitSaveJobset(jobset);
+
+%% Change jobs information if required
+
+if opts.andJobs
+    
+    % get job structure
+    jobs = kitGenerateMovieStructs(jobset);
+    
+    % for each job in the jobset, change the chromatic shift information
+    for iMov = 1:length(jobs)
+        jobs(iMov).options.chrShift.result = chrShift;
+        jobs(iMov) = kitSaveJob(jobs(iMov));
+    end
+else
+    jobs = NaN;
+end
+
+end
+
+
+
+
