92899c439aefc25c598c27795acbdf76d3425cc8
Chris <chris@mechanochemistry.org>
2017-07-03 16:05:19 +0100

    Some updates to chromatic shift calculations, so that the calculated chromatic shift is output to the command line. Overhaul to the way chromatic shift is changed. Also directories are requested for files from which the new chromatic shift is calculated.

--- chrsCalculateChromaticShift.m
+++ chrsCalculateChromaticShift.m
@@ -1,4 +1,4 @@
-function [chrShift,nDataPoints] = chrsCalculateChromaticShift(jobs,chanVect,varargin)
+function [chrShift,nDataPoints] = chrsCalculateChromaticShift(data,chanVect,varargin)
 % CHRSCALCULATECHROMATICSHIFT Produces a vector for chromatic shift from a
 %    set of images previously processed for chromatic shift measurement.
 %
@@ -19,7 +19,7 @@ function [chrShift,nDataPoints] = chrsCalculateChromaticShift(jobs,chanVect,vara
 %         the McAinsh ultraView spinning disk microscope.
 %
 %
-% Copyright (C) 2015 C. A. Smith
+% Copyright (C) 2017 C. A. Smith
 
 % set default options
 opts.filtered = 1;
@@ -27,20 +27,60 @@ opts.interphaseToMetaphase = 0;
 % process options
 opts = processOptions(opts,varargin{:});
 
+% if data is a zetaStructure, provide results from this
+if isstruct(data) && isfield(data,'chrsVersion')
+    
+    kitLog('Pre-processed zetaStructure detected.')
+    
+    chrShift = zeros(1,6);
+    if opts.filtered
+        % find median and standard deviation of chromatic shift measurements
+        chrShift(1) = nanmedian(data.filtered.zeta.x(:));
+        chrShift(2) = nanmedian(data.filtered.zeta.y(:));
+        chrShift(3) = nanmedian(data.filtered.zeta.z(:));
+        chrShift(4) = nanstd(data.filtered.zeta.x(:));
+        chrShift(5) = nanstd(data.filtered.zeta.y(:));
+        chrShift(6) = nanstd(data.filtered.zeta.z(:));
+        nDataPoints = sum(~isnan(data.filtered.zeta.x));
+    else
+        % find median and standard deviation of chromatic shift measurements
+        chrShift(1) = nanmedian(data.raw.zeta.x(:));
+        chrShift(2) = nanmedian(data.raw.zeta.y(:));
+        chrShift(3) = nanmedian(data.raw.zeta.z(:));
+        chrShift(4) = nanstd(data.raw.zeta.x(:));
+        chrShift(5) = nanstd(data.raw.zeta.y(:));
+        chrShift(6) = nanstd(data.raw.zeta.z(:));
+        nDataPoints = sum(~isnan(data.raw.zeta.x));
+    end
+    % give a warning in the case that there are less than 20 datapoints
+    if nDataPoints < 20
+        warning('Have very few datapoints across all chromatic shift movies. Consider revising.')
+    end
+    %correct z-coordinate from interphase to metaphase if necessary
+    if opts.interphaseToMetaphase
+        chrShift(3) = chrShift(3) + 0.0782; % correct for McA spinning disk dual camera
+    end
+    
+    kitLog('Chromatic shift calculated: [%.1f %.1f %.1f] ± [%.1f %.1f %.1f] (n = %i)',...
+        chrShift(1)*1000,chrShift(2)*1000,chrShift(3)*1000,chrShift(4)*1000,chrShift(5)*1000,chrShift(6)*1000,nDataPoints)
+    return
+    
+end
+
 % check that a channel vector has been provided
 if nargin<2 || isempty(chanVect)
     chanVect = [1 2];
 end
 
 % find number of jobs
-nJobs = length(jobs);
+nJobs = length(data);
 % preallocate vector for accumulating chromatic shift measurements, zeta
 accumZeta = [];
 
 % loop over jobs
 for iJob = 1:nJobs
     % get number of channels
-    nChans = length(jobs{iJob}.dataStruct);
+    nChans = length(data{iJob}.dataStruct);
     % check that this is enough based on the provided channel vector
     if any(chanVect>nChans)
         chrShift = zeros(1,6);
@@ -49,25 +89,25 @@ for iJob = 1:nJobs
     end
     
     % get coordinates, calculate zeta
-    if isfield(jobs{iJob}.dataStruct{chanVect(2)},'initCoord') && ~jobs{iJob}.dataStruct{chanVect(2)}.failed
+    if isfield(data{iJob}.dataStruct{chanVect(2)},'initCoord') && ~data{iJob}.dataStruct{chanVect(2)}.failed
       
       % get coordinates for each channel
       if opts.filtered
-        coords1 = jobs{iJob}.dataStruct{chanVect(1)}.initCoord.allCoord(:,1:3);
-        coords2 = jobs{iJob}.dataStruct{chanVect(2)}.initCoord.allCoord(:,1:3);
+        coords1 = data{iJob}.dataStruct{chanVect(1)}.initCoord.allCoord(:,1:3);
+        coords2 = data{iJob}.dataStruct{chanVect(2)}.initCoord.allCoord(:,1:3);
       else
         % check for a rawInitCoord in each channel dataStruct
         % channel 1
-        if isfield(jobs{iJob}.dataStruct{chanVect(1)},'rawInitCoord')
-            coords1 = jobs{iJob}.dataStruct{chanVect(1)}.rawInitCoord.allCoord(:,1:3);
+        if isfield(data{iJob}.dataStruct{chanVect(1)},'rawInitCoord')
+            coords1 = data{iJob}.dataStruct{chanVect(1)}.rawInitCoord.allCoord(:,1:3);
         else
-            coords1 = jobs{iJob}.dataStruct{chanVect(1)}.initCoord.allCoord(:,1:3);
+            coords1 = data{iJob}.dataStruct{chanVect(1)}.initCoord.allCoord(:,1:3);
         end
         % channel 2
-        if isfield(jobs{iJob}.dataStruct{chanVect(2)},'rawInitCoord')
-            coords2 = jobs{iJob}.dataStruct{chanVect(2)}.rawInitCoord.allCoord(:,1:3);
+        if isfield(data{iJob}.dataStruct{chanVect(2)},'rawInitCoord')
+            coords2 = data{iJob}.dataStruct{chanVect(2)}.rawInitCoord.allCoord(:,1:3);
         else
-            coords2 = jobs{iJob}.dataStruct{chanVect(2)}.initCoord.allCoord(:,1:3);
+            coords2 = data{iJob}.dataStruct{chanVect(2)}.initCoord.allCoord(:,1:3);
         end
       end
       
@@ -93,8 +133,11 @@ else
     chrShift(4:6) = nanstd(accumZeta);
     %correct z-coordinate from interphase to metaphase if necessary
     if opts.interphaseToMetaphase
-        chrShift(3) = chrShift(3) + 0.0782; % correct for McA spinning disk
+        chrShift(3) = chrShift(3) + 0.0782; % correct for McA spinning disk dual camera
     end
 end
+
+kitLog('Chromatic shift calculated: [%.1f %.1f %.1f] ± [%.1f %.1f %.1f] (n = %i)',...
+        chrShift(1)*1000,chrShift(2)*1000,chrShift(3)*1000,chrShift(4)*1000,chrShift(5)*1000,chrShift(6)*1000,nDataPoints)
 
 end
--- chrsChangeChromaticShift.m
+++ chrsChangeChromaticShift.m
@@ -14,11 +14,9 @@ function [jobset,jobs] = chrsChangeChromaticShift(jobset,newShift,varargin)
 %
 %    andJobs: {0} or 1. Whether or not to correct all jobs already
 %         processed.
-%         NB: This is not recommended for significant changes in chromatic
-%         shift measurements.
 %
 %
-% Copyright (c) 2016 C. A. Smith
+% Copyright (c) 2017 C. A. Smith
 
 % default options
 opts.chanVect = [];
@@ -39,8 +37,12 @@ if isempty(opts.chanVect)
         for fromChan=1:2
           for toChan=2:3
             if fromChan<toChan && sum(chrShift{fromChan,toChan})~=0
-              source{fromChan,toChan} = 'Unknown source';
-              source{toChan,fromChan} = 'Unknown source';
+              % want to ask user to select the chromatic shift-derived jobset
+              kitLog('Please choose a jobset pertaining to the chromatic shift between channels %i and %i:',fromChan,toChan);
+              [filename,~] = uigetfile('*.mat','Select jobset file');
+              kitLog('%s selected.',filename);
+              source{fromChan,toChan} = filename;
+              source{toChan,fromChan} = filename;
             else
               source{fromChan,toChan} = [];
               source{toChan,fromChan} = [];
@@ -50,7 +52,7 @@ if isempty(opts.chanVect)
     end
     
 % check whether the channel vector includes only allowed channels
-elseif ~isempty(setdiff(opts.chanVect,1:3))
+elseif any(~ismember(opts.chanVect,1:3))
     error('Can only choose channels from 1 to 3.')
     
 else % if channel vector provided, proceed as appropriate
@@ -77,32 +79,227 @@ else % if channel vector provided, proceed as appropriate
     revShift(1:3) = revShift(1:3)*-1;
     chrShift{toChan,fromChan}(1:lenShift) = revShift;
     
-    % give a source for the chromatic shift calculation
-    source{fromChan,toChan} = 'Unknown source';
-    source{toChan,fromChan} = 'Unknown source';
+    % want to ask user to select the chromatic shift-derived jobset
+    kitLog('Please choose a jobset pertaining to the chromatic shift between channels %i and %i:',fromChan,toChan);
+    [filename,~] = uigetfile('*.mat','Select jobset file');
+    kitLog('%s selected.',filename);
+    source{fromChan,toChan} = filename;
+    source{toChan,fromChan} = filename;
     
 end
 
 % change chromatic shift information in the jobset and save
 jobset.options.chrShift.result = chrShift;
 jobset.options.chrShift.jobset = source;
-kitSaveJobset(jobset);
+% kitSaveJobset(jobset);
 
 %% Change jobs information if required
 
 if opts.andJobs
     
     % get job structure
-    jobs = kitGenerateMovieStructs(jobset);
+    jobs = kitLoadAllJobs(jobset);
     
     % for each job in the jobset, change the chromatic shift information
     for iMov = 1:length(jobs)
-        jobs(iMov).options.chrShift.result = chrShift;
-        jobs(iMov) = kitSaveJob(jobs(iMov));
+        
+        % get some basic metadata
+        oldChrShift = jobs{iMov}.options.chrShift.result;
+        % change the chromatic shift information
+        jobs{iMov}.options.chrShift.result = chrShift;
+            
+        if isempty(opts.chanVect)
+            
+            for fromChan = 1:2
+                for toChan = 2:3
+                    
+                    % if no change, skip
+                    if isequal(chrShift{fromChan,toChan}(:,1:3),oldChrShift{fromChan,toChan}(:,1:3));
+                        break
+                    end
+                    
+                    % calculate the difference between new and old chromatic shift measurements
+                    diffChrShift = chrShift{fromChan,toChan}(:,1:3) - oldChrShift{fromChan,toChan}(:,1:3);
+
+                    % check for initCoord measures, and change accordingly
+                    if ~(length(jobs{iMov}.dataStruct) < toChan) && isfield(jobs{iMov}.dataStruct{toChan},'failed') ...
+                            && ~jobs{iMov}.dataStruct{toChan}.failed ...
+                            && ~isempty(jobs{iMov}.dataStruct{toChan}.initCoord(1).allCoord)
+
+                        % get dataStruct, and shift the data
+                        dataStruct = jobs{iMov}.dataStruct{toChan};
+                        dataStruct = shiftData(dataStruct,diffChrShift,jobs{iMov}.metadata);
+
+                    end
+
+                    % save dataStruct
+                    jobs{iMov}.dataStruct{toChan} = dataStruct;
+                    if isfield(dataStruct,'tracks')
+                        jobs{iMov} = kitExtractTracks(jobs{iMov},toChan);
+                    end
+
+                end
+            end
+            
+        else
+            
+            % if no change, skip
+            if isequal(chrShift{fromChan,toChan}(:,1:3),oldChrShift{fromChan,toChan}(:,1:3))
+                break
+            end
+
+            % calculate the difference between new and old chromatic shift measurements
+            diffChrShift = chrShift{fromChan,toChan}(:,1:3) - oldChrShift{fromChan,toChan}(:,1:3);
+                        
+            % check for initCoord measures, and change accordingly
+            if ~(length(jobs{iMov}.dataStruct) < toChan) && isfield(jobs{iMov}.dataStruct{toChan},'failed') ...
+                    && ~jobs{iMov}.dataStruct{toChan}.failed ...
+                    && ~isempty(jobs{iMov}.dataStruct{toChan}.initCoord(1).allCoord)
+                
+                % get dataStruct, and shift the data
+                dataStruct = jobs{iMov}.dataStruct{toChan};
+                dataStruct = shiftData(dataStruct,diffChrShift,jobs{iMov}.metadata);
+                
+            end
+            
+            % save dataStruct
+            jobs{iMov}.dataStruct{toChan} = dataStruct;
+            if isfield(dataStruct,'tracks')
+                jobs{iMov} = kitExtractTracks(jobs{iMov},toChan);
+            end
+            
+        end
+        
+%         kitSaveJob(jobs{iMov});
+        
     end
 else
     jobs = NaN;
 end
+
+end
+
+%% SUBFUNCTIONS
+
+function dataStruct = shiftData(dataStruct,diffChrShift,metadata)
+
+    % get all dataStruct information
+    initCoord = dataStruct.initCoord; % initCoord
+    if isfield(dataStruct,'planeFit') % planeFit
+        planeFit = dataStruct.planeFit;
+    else
+        planeFit = [];
+    end
+    if isfield(dataStruct,'tracks') % tracks
+        tracks = dataStruct.tracks;
+        nTracks = length(tracks);
+    else
+        tracks = [];
+        nTracks = 0;
+    end
+    if isfield(dataStruct,'sisterList') && ~isempty(dataStruct.sisterList(1).trackPairs) % sisterList
+        sisterList = dataStruct.sisterList;
+        nSisters = length(sisterList);
+    else
+        sisterList = struct('trackPairs',[]);
+        nSisters = 0;
+    end
+    
+    % get some metadata
+    pixelSize = metadata.pixelSize(1:3);
+	nFrames = metadata.nFrames;
+
+    % find which frames have a plane
+    framesWiPlane = [];
+    for iFrame = 1:nFrames
+        if ~isempty(planeFit) && planeFit(iFrame).planeVectorClassifier
+            framesWiPlane = [framesWiPlane iFrame];
+        end
+    end
+    % create a rotated chromatic shift structure for later
+    rotChrShift = repmat(diffChrShift,nFrames,1);
+    % get all coordinate information
+    if length(framesWiPlane) > 1
+
+        %get first and last frames to rotate
+        firstFrameRotate = framesWiPlane(1);
+        lastFrameRotate = framesWiPlane(end);
+
+        %find frames without plane that are before the first frame with plane
+        framesBefore1 = 1:firstFrameRotate-1;
+        framesAfter1 = lastFrameRotate+1:nFrames;
+
+        %get the coordinate system of each frame with a plane
+        coordSystem = zeros(3,3,nFrames);
+        coordSystem(:,:,framesWiPlane) = cat(3,planeFit.planeVectors);
+        % give any frames without a coordinate system the same as the nearest with a plane
+        for iTime = framesBefore1(end:-1:1)
+            coordSystem(:,:,iTime) = coordSystem(:,:,iTime+1);
+        end
+        for iTime = framesAfter1
+            coordSystem(:,:,iTime) = coordSystem(:,:,iTime-1);
+        end
+
+    else
+        % give a standard rotation-free coordinate system
+        coordSystem = zeros(3,3,nFrames);
+    end
+
+    % shift and rotate initCoord
+    for iFrame = 1:nFrames
+
+        % get number of spots, then add the chromatic shift difference to initCoords
+        nSpots = initCoord(iFrame).nSpots;
+        if nSpots == 0
+            continue
+        end
+        initCoord(iFrame).allCoord(:,1:3) = initCoord(iFrame).allCoord(:,1:3) ...
+            + repmat(diffChrShift,nSpots,1);
+        initCoord(iFrame).allCoordPix(:,1:3) = initCoord(iFrame).allCoordPix(:,1:3) ...
+            + repmat(diffChrShift./pixelSize,nSpots,1);
+
+        % change plane fit
+        if sum(coordSystem(:,:,iFrame)==0)==3
+            planeFit(iFrame).rotatedCoord = initCoord(iFrame).allCoord;
+            planeFit(iFrame).rotatedCoord(:,1:3) = initCoord(iFrame).allCoord(:,1:3) - planeFit(iFrame).planeOrigin(:,1:3);
+        else
+            rotationMat = inv(coordSystem(:,:,iFrame)); %rotation matrix
+            errorPropMat = rotationMat.^2; %error propagation matrix
+            planeFit(iFrame).rotatedCoord(:,1:3) = (rotationMat*(initCoord(iFrame).allCoord(:,1:3))')';
+            planeFit(iFrame).rotatedCoord(:,4:6) = sqrt((errorPropMat*((initCoord(iFrame).allCoord(:,4:6)).^2)')');
+            % form a rotational chromatic shift correction
+            rotChrShift(iFrame,1:3) = (rotationMat*(diffChrShift(:,1:3))')';
+        end
+
+    end
+
+    % chromatic shift correct tracks
+    for iTrack = 1:nTracks
+        for iCoord = 1:3
+            tracks(iTrack).tracksCoordAmpCG(:,iCoord:8:end) = tracks(iTrack).tracksCoordAmpCG(:,iCoord:8:end) ...
+                + diffChrShift(iCoord);
+            startTime = tracks(iTrack).seqOfEvents(1,1);
+            endTime = tracks(iTrack).seqOfEvents(2,1);
+            tracks(iTrack).coordAmp4Tracking(:,iCoord:8:end) = tracks(iTrack).coordAmp4Tracking(:,iCoord:8:end) ...
+                + rotChrShift(startTime:endTime,iCoord)';
+        end
+    end
+
+    % chromatic shift correct sisterList
+    for iSis = 1:nSisters
+        sisterList(iSis).coords1(:,1:3) = sisterList(iSis).coords1(:,1:3) ...
+            + rotChrShift(:,1:3);
+        sisterList(iSis).coords2(:,1:3) = sisterList(iSis).coords2(:,1:3) ...
+            + rotChrShift(:,1:3);
+    end
+
+    % save initCoord, planeFit and tracks information
+    dataStruct.initCoord = initCoord;
+    dataStruct.planeFit = planeFit;
+    if ~isempty(tracks)
+        dataStruct.tracks = tracks;
+        dataStruct.sisterList = sisterList;
+    end
 
 end
 
