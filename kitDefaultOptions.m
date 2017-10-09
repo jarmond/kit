6ac5912a60a43b10da54f8c62d9e3fd2d151b8cc
Chris Smith <c.a.smith.2@hotmail.co.uk>
2017-10-10 00:02:54 +0100

    kitGUI renamed to kitSetupJob: Overhauled to reduce the visible options. Options are now tabular.

--- kitDefaultOptions.m
+++ kitDefaultOptions.m
@@ -15,29 +15,100 @@ job.matlabVersion = version;
 
 job.movieDirectory = [];
 job.movieFiles = [];
+opts.groupOutput = 0;
 
 opts.jobProcess = 'zandt';
-opts.spotMode{1} = 'histcut'; % Possible values: histcut, wavelet, neighbour, manual, none
-opts.spotMode{2} = 'none';
+opts.spotMode{1} = 'histcut'; % Possible values: histcut, manual, none
+opts.spotMode{2} = 'neighbour';
 opts.spotMode{3} = 'none';
+opts.spotMode{4} = 'none';
 opts.coordMode{1} = 'gaussian'; % Possible values: centroid, gaussian, none
-opts.coordMode{2} = 'none';
+opts.coordMode{2} = 'gaussian';
 opts.coordMode{3} = 'none';
-opts.coordSystem = 'plate'; % Possible values: plate, image, com
+opts.coordMode{4} = 'none';
+opts.coordSystem = 'plate'; % Possible values: plate, image, com, none
 opts.coordSystemChannel = 1;
-opts.cropAsked = 0;
 
 %Chromatic shift information
-chrShift.result = repmat({zeros(1,6)},3,3);
-chrShift.jobset = repmat({[]},3,3);
-for i=1:3; for j=1:3; chrShift.chanOrder{i,j} = [i j]; end; end
-chrShift.coordinateAdjustments = repmat({zeros(1,3)},3,3);
+chrShift.result = repmat({zeros(1,6)},4,4);
+chrShift.jobset = repmat({[]},4,4);
+for i=1:4; for j=1:4; chrShift.chanOrder{i,j} = [i j]; end; end
+chrShift.coordinateAdjustments = repmat({zeros(1,3)},4,4);
 chrShift.minSpots = 20;
 chrShift.filtering = 0;
-chrShift.intensityFilter = 0; % ratio of spot intensity with max intensity
-chrShift.neighbourFilter = 0; % in um, min distance between spots
+chrShift.intensityFilter = 0.25; % ratio of spot intensity with max intensity
+chrShift.neighbourFilter = 0.75; % in um, min distance between spots
 chrShift.regionFilter = 1; % number of regions to divide chromatic shift images
 opts.chrShift = chrShift;
+
+% Maki options.
+opts.minSpotsPerFrame = 20;
+opts.maxSpotsPerFrame = 150;
+opts.betterBackground = 0; % 1 == mask signal before taking background.
+opts.fitCloud = 0; % 1 == use max evector as normal axis.
+opts.robustStats = 0;
+opts.maxCloseGap = 4;
+opts.autoRadiidt = 2;
+opts.autoRadiiAvgDisp = 0.06;
+opts.minSearchRadius = [0.01 0.1 0.01]; % [inliers, unaligned, lagging] um
+opts.maxSearchRadius = [0.75 3 0.75]; % [inliers, unaligned, lagging] um
+opts.useSisterAlignment = 1;
+opts.maxSisterAlignmentAngle = 30; % degrees
+opts.maxSisterSeparation = 1.5; % um
+opts.minSisterTrackOverlap = 10; % percent of movie length to require overlap
+opts.oppositeAnaphaseDir = 1; % Use assumption of opposition direction in anaphase.
+
+direction.assignMode = 'voting'; % directional (AP or P) assignment, can also be 'absolute'
+direction.assignExpWeight = 1; % exponentially weight displacements
+direction.minConsSteps = 3; % minimum consecutive timepoints in one direction
+direction.switchBuffer = 1; % number of timepoints at start and end of run
+                          % to be considered directional switches
+opts.direction = direction;
+
+% Gaussian mixture-model spot finding options.
+mmf.clusterSeparation = 5; % in PSF sigmas. If too large will fit whole
+                            % plate, if too small will not account for
+                            % overlapping PSFs.
+mmf.alphaF = [0.05 0.05 0.05 0.05]; % N vs N+1 F-test cutoff.
+mmf.alphaA = [0.05 0.075 0.10 0.10]; % amplitude t-test cutoff.
+mmf.alphaD = [0.01 0.01 0.01 0.01]; % distance t-test cutoff.
+mmf.mmfTol = 1e-5; % accuracy to which MMF fits Gaussians.
+mmf.oneBigCluster = 0; % fit all spots together as one big cluster
+mmf.maxMmfTime = 300; % Maximum per-frame time (sec) to attempt mixture model fit
+                      % before giving up.  Use zero to disable.
+mmf.addSpots = 0; % Try fitting multiple Gaussians to spots to identify overlaps
+opts.mmf = mmf;
+
+% Image moment coordinate system.
+opts.momentPrctile = 99; % percentile to threshold image at before computing
+                         % moments
+
+% Neighbouring spot localisation.
+neighbourSpots.maskRadius = 0.3; % um
+neighbourSpots.maskShape = 'semicirc';
+neighbourSpots.maskConeAngle = 10; % degrees, only used with maskShape=='cone'.
+neighbourSpots.channelOrientation = 1:4; % from inner to outer
+neighbourSpots.timePoints = repmat({[]},1,4);
+neighbourSpots.zSlices = repmat({[]},1,4);
+opts.neighbourSpots = neighbourSpots;
+
+% Local spot intensity.
+intensity.execute = zeros(1,4); % whether or not to run intensity measurements per channel
+intensity.maskRadius = 0.3; % um
+intensity.photobleachCorrect = 1;
+intensity.attachmentCorrect = 1;
+intensity.maskShape = 'circle';
+intensity.maskConeAngle = 10; % degrees, only used with maskShape=='cone'.
+intensity.poleShift = 0; % um
+intensity.otherSpotSearchRadius = 0;
+intensity.gaussFilterSpots = 0;
+opts.intensity = intensity;
+
+% Manual spot detection options.
+manualDetect.warningDist = 0.2; % in um, distance between spots below which to warn user
+manualDetect.frameSpacing = 7; % number of frames between adjacent manual detection frame
+manualDetect.gapMethod = 'framewise'; % can also be 'linear'
+opts.manualDetect = manualDetect;
 
 % Debug options.
 debug.showMmfClusters = 0; % visualize clustering from overlapping PSFs, -1
@@ -58,90 +129,5 @@ debug.showAdaptive = 0; % 1 to show adaptive thresholding algorithm verbosely.
 debug.asserts = 0; % check things that shouldn't go wrong
 debug.disableSave = 0; % don't save job at each step
 opts.debug = debug;
-
-
-% Maki options.
-opts.minSpotsPerFrame = 20;
-opts.maxSpotsPerFrame = 1000;
-opts.betterBackground = 0; % 1 == mask signal before taking background.
-opts.fitCloud = 0; % 1 == use max evector as normal axis.
-opts.robustStats = 0;
-opts.maxCloseGap = 4;
-opts.autoRadiidt = 2;
-opts.minSearchRadius = [0.01 0.1 0.01]; % [inliers, unaligned, lagging] um
-opts.maxSearchRadius = [0.75 3 0.75]; % [inliers, unaligned, lagging] um
-opts.useSisterAlignment = 1;
-opts.maxSisterAlignmentAngle = 30; % degrees
-opts.maxSisterSeparation = 1.5; % um
-opts.minSisterTrackOverlap = 10; % percent of movie length to require overlap
-opts.oppositeAnaphaseDir = 1; % Use assumption of opposition direction in
-                                 % anaphase.
-
-direction.assignMode = 'voting'; % directional (AP or P) assignment, can also be 'absolute'
-direction.assignExpWeight = 1; % exponentially weight displacements
-direction.minConsSteps = 3; % minimum consecutive timepoints in one direction
-direction.switchBuffer = 1; % number of timepoints at start and end of run
-                          % to be considered directional switches
-opts.direction = direction;
-
-% Gaussian mixture-model spot finding options.
-mmf.clusterSeparation = 5; % in PSF sigmas. If too large will fit whole
-                            % plate, if too small will not account for
-                            % overlapping PSFs.
-mmf.alphaF = [0.05 0.05 0.05]; % N vs N+1 F-test cutoff.
-mmf.alphaA = [0.05 0.075 0.10]; % amplitude t-test cutoff.
-mmf.alphaD = [0.01 0.01 0.01]; % distance t-test cutoff.
-mmf.mmfTol = 1e-5; % accuracy to which MMF fits Gaussians.
-mmf.oneBigCluster = 0; % fit all spots together as one big cluster
-mmf.maxMmfTime = 300; % Maximum per-frame time (sec) to attempt mixture model fit
-                      % before giving up.  Use zero to disable.
-mmf.addSpots = 0; % Try fitting multiple Gaussians to spots to identify overlaps
-opts.mmf = mmf;
-
-% Image moment coordinate system.
-opts.momentPrctile = 99; % percentile to threshold image at before computing
-                         % moments
-
-% Neighbouring spot localisation.
-neighbourSpots.maskRadius = 0.3; % um
-neighbourSpots.maskShape = 'semicirc';
-neighbourSpots.maskConeAngle = 10; % degrees, only used with maskShape=='cone'.
-neighbourSpots.channelOrientation = 1:3; % from inner to outer
-neighbourSpots.timePoints = repmat({[]},1,3);
-neighbourSpots.zSlices = repmat({[]},1,3);
-opts.neighbourSpots = neighbourSpots;
-
-% Local spot intensity.
-intensity.execute = zeros(1,3); % whether or not to run intensity measurements per channel
-intensity.maskRadius = 0.3; % um
-intensity.photobleachCorrect = 1;
-intensity.attachmentCorrect = 1;
-intensity.maskShape = 'circle';
-intensity.maskConeAngle = 10; % degrees, only used with maskShape=='cone'.
-intensity.poleShift = 0; % um
-intensity.otherSpotSearchRadius = 0;
-intensity.gaussFilterSpots = 0;
-opts.intensity = intensity;
-
-% Adaptive threshold spot detection.
-opts.adaptiveLambda = 10; % Regularization for increasing number of spots.
-
-% Deconvolution.
-opts.deconvolve = 0;
-
-% Wavelet multiscale product spot detector options.
-wavelet.levelThresh = 2; % threshold scale for local MAD thresholding
-wavelet.levelAdapt = 1; % use adaptive setting for above.
-wavelet.numLevels = 3;  % number of wavelet levels
-wavelet.localMAD = 0; % locally estimated MAD
-wavelet.backSub = 0;  % background subtraction
-wavelet.minLevel = 1; % discard wavelet levels below this
-opts.wavelet = wavelet;
-
-% Manual spot detection options.
-manualDetect.warningDist = 0.2; % in um, distance between spots below which to warn user
-manualDetect.frameSpacing = 8; % number of frames between adjacent manual detection frame
-manualDetect.gapMethod = 'framewise'; % can also be 'linear'
-opts.manualDetect = manualDetect;
 
 job.options = opts;
