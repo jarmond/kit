eaf7f24bf0d2857f6fbde8af749cbb23a9e1c50e
Chris Smith <chrissmith.software@gmail.com>
2018-08-07 11:36:34 +0100

    Was calculating transpose, now corrected.

--- kitComputeStackSize.m
+++ kitComputeStackSize.m
@@ -8,11 +8,11 @@ else
   h = crop(3);
   x1 = crop(2);
   y1 = crop(1);
-  x2 = min(round(x1 + w),frameSize(1));
-  y2 = min(round(y1 + h),frameSize(2));
+  x2 = min(round(x1 + w),frameSize(2));
+  y2 = min(round(y1 + h),frameSize(1));
   x1 = max(round(x1),1);
   y1 = max(round(y1),1);
   sx = x2-x1+1;
   sy = y2-y1+1;
-  stackSize = [sx,sy,frameSize(3)];
+  stackSize = [sy,sx,frameSize(3)];
 end
