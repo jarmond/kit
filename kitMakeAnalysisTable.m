5b9fce1de7220d5af21a24653672af16defa5171
Jonathan Harrison <jonathan.u.harrison@warwick.ac.uk>
2019-04-15 14:57:57 +0100

    Updated kitMakeAnalysisTable to work on jobsets with multiple jobs

--- kitMakeAnalysisTable.m
+++ kitMakeAnalysisTable.m
@@ -1,4 +1,4 @@
-function T = kitMakeAnalysisTable(job,channel)
+function analysisTable = kitMakeAnalysisTable(jobset,channel)
 %%
 %%Take a processed, paired jobset file and convert analysis to a convenient
 %%table format
@@ -10,60 +10,86 @@ if nargin <2
     channel=1;
 end
 
-if ~isfield(job,'dataStruct')
-    error('Expect a jobset file that has already been processed and paired');
+%get the data
+job = kitLoadAllJobs(jobset);
+nMovs = length(job);
+
+nFrames = job{1}.metadata.nFrames;
+if nFrames > 1 %treat time sereies and single time points differently
+    varnames = {'Position','Amplitude','Frame','SisterPairID','SisterID'};
+    analysisTable = cell2table(cell(0,6),cat(2,varnames,'movieID'));
+else
+    varnames = {'Position','Amplitude'};
+    if isfield(job{1},'categories')
+        numVars = 3 + numel(fieldnames(job{1}.categories));
+        analysisTable = cell2table(cell(0,numVars),'VariableNames',cat(2,varnames,fieldnames(job{1}.categories)),'movieID');
+    else
+        numVars = 3;
+        analysisTable = cell2table(cell(0,numVars),'VariableNames',cat(2,varnames,'movieID'));
+    end
 end
-
-dataStruct = job.dataStruct{channel};
-
-nFrames = job.metadata.nFrames;
-if nFrames > 1
-    warning('Categories cannot yet be used for movies. Making a basic table')
-    if isfield(dataStruct,'sisterList')         
-        nSisters = length(dataStruct.sisterList);
-        %create columns for table
-        position = zeros(nFrames*nSisters*2,3);
-        amplitude = zeros(nFrames*nSisters*2,3);
-        frame = repmat(repmat((1:nFrames)',nSisters,1),2,1);
-        sisterPairID = repmat(1:nSisters,nFrames,1);
-        sisterPairID = repmat(sisterPairID(:),2,1);
-        sisterID = [ones(nFrames*nSisters,1);2*ones(nFrames*nSisters,1)];
-        for k = [1,2]
-            for j = 1:nSisters
-                for i = 1:nFrames
-                    if k==1 %use coords1 or coords2
-                        position((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
-                            dataStruct.sisterList(j).coords1(i,1:3);
-                        amplitude((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
-                            dataStruct.sisterList(j).coords1(i,4:6);
-                    elseif k==2
-                        position((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
-                            dataStruct.sisterList(j).coords2(i,1:3);
-                        amplitude((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
-                            dataStruct.sisterList(j).coords2(i,4:6); 
+for jobInd = 1:nMovs
+    dataStruct = job{jobInd}.dataStruct{channel};
+    
+    if ~isfield(job{jobInd},'dataStruct')
+        error('Expect a jobset file that has already been processed and paired');
+    end
+    
+    if ~isfield(dataStruct,'failed') || dataStruct.failed
+        warning('Movie %d has failed so skipping \n',jobInd);
+        continue
+    end
+    
+    nFrames = job{jobInd}.metadata.nFrames;
+    if nFrames > 1
+        warning('Categories cannot yet be used for movies. Making a basic table')
+        if isfield(dataStruct,'sisterList')
+            nSisters = length(dataStruct.sisterList);
+            %create columns for table
+            position = zeros(nFrames*nSisters*2,3);
+            amplitude = zeros(nFrames*nSisters*2,3);
+            frame = repmat(repmat((1:nFrames)',nSisters,1),2,1);
+            sisterPairID = repmat(1:nSisters,nFrames,1);
+            sisterPairID = repmat(sisterPairID(:),2,1);
+            sisterID = [ones(nFrames*nSisters,1);2*ones(nFrames*nSisters,1)];
+            for k = [1,2]
+                for j = 1:nSisters
+                    for i = 1:nFrames
+                        if k==1 %use coords1 or coords2
+                            position((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
+                                dataStruct.sisterList(j).coords1(i,1:3);
+                            amplitude((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
+                                dataStruct.sisterList(j).coords1(i,4:6);
+                        elseif k==2
+                            position((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
+                                dataStruct.sisterList(j).coords2(i,1:3);
+                            amplitude((k-1)*nFrames*nSisters + (j-1)*nFrames + i,:) = ...
+                                dataStruct.sisterList(j).coords2(i,4:6);
+                        end
                     end
                 end
             end
+            
+            T = table(position,amplitude,frame,sisterPairID,sisterID,...
+                'VariableNames', varnames);
+        else
+            error('No sisterList found but need paired joblist to make table from');
         end
-
-        varnames = {'Position','Amplitude','Frame','SisterPairID','SisterID'};
-        T = table(position,amplitude,frame,sisterPairID,sisterID,...
-            'VariableNames', varnames);
     else
-        error('No sisterList found but need paired joblist to make table from');
-    end
-else
-    %put all spots into a table structure
-    T = table(dataStruct.initCoord.allCoord(:,1:3), ...
-        dataStruct.initCoord.allCoord(:,4:6), ...
-        'VariableNames',{'Position','Amplitude'});
-    % add category information from manual labelling via gui
-    if isfield(job,'categories') %have added some kind of categories
-        categoryNames = fieldnames(job.categories);
-        for j=1:numel(fieldnames(job.categories))
-            T.(categoryNames{1}) = zeros(size(T,1),1);
-            T.(categoryNames{1})(job.categories.(categoryNames{1})) = 1;
+        %put all spots into a table structure
+        T = table(dataStruct.initCoord.allCoord(:,1:3), ...
+            dataStruct.initCoord.allCoord(:,4:6), ...
+            'VariableNames',{'Position','Amplitude'});
+        % add category information from manual labelling via gui
+        if isfield(job,'categories') %have added some kind of categories
+            categoryNames = fieldnames(job.categories);
+            for j=1:numel(fieldnames(job.categories))
+                T.(categoryNames{1}) = zeros(size(T,1),1);
+                T.(categoryNames{1})(job.categories.(categoryNames{1})) = 1;
+            end
         end
     end
+    T.movieID = jobInd*ones(size(T,1),1);
+    analysisTable = [analysisTable; T];
 end
 
