e1f1a9312a07735c446c2b92ef6176ec2c15a409
jonathan harrison <jonathan.u.harrison@warwick.ac.uk>
2020-01-28 13:17:38 +0000

    Edit path to account for cropping

--- jonathanEstimatePSFFromBeadImages.m
+++ jonathanEstimatePSFFromBeadImages.m
@@ -1,5 +1,5 @@
 %estimate psf for every available bead image and write to appropriate file
-bead_file_struct = dir('/run/user/1001/gvfs/smb-share:server=ads.warwick.ac.uk,share=shared/HCSS1/Shared299/JHOS/PSF BEAD IMAGES_correct/*.ome.tif');
+bead_file_struct = dir('/run/user/1001/gvfs/smb-share:server=ads.warwick.ac.uk,share=shared/HCSS1/Shared299/JHOS/PSF BEAD IMAGES_correct/cropped/*.ome.tif');
 for j = 1:length(bead_file_struct)
     pathToMovie = fullfile(bead_file_struct(j).folder,bead_file_struct(j).name);
     jonathanEstimateSigma(pathToMovie,0,1);
