ffc5ccc415882b989e2e1aea9c11c17ee6cfffd1
Jonathan Harrison <jonathan.u.harrison@warwick.ac.uk>
2020-01-28 14:26:07 +0000

    Added file to make jobset files in pipeline

---
+++ jonathanPipelineJobsetSetup.m
@@ -0,0 +1,62 @@
+function jobset = jonathanPipelineJobsetSetup(movieDir,identifier)
+%%make custom kit jobset files for tracking in pipeline
+%% in most cases not a replacement for using kitSetupJob
+%% but helpful when we want to automate creating many standard jobset files
+%%J.U. harrison 2020-01-28
+%%
+%%%%%%%%
+
+if nargin<2
+    identifier = 'DonaldDuck_auto_v101';
+end
+
+if nargin<1
+    movieDir = '/Volumes/shared/HCSS1/Shared299/JHOS/Reprocessing_nocodazole_washout/OS_LLSM_191119_MC191_DMSO_washout';
+end
+
+jobset = kitDefaultOptions();
+jobset.options.jobProcess='zandt';
+
+if ~isfield(jobset,'jobsetVersion') || ...
+        jobset.jobsetVersion < kitVersion(2)
+    jobset = kitJobset(jobset);
+end
+
+if ~isfield(jobset,'ROI')
+    jobset.ROI = [];
+end
+
+jobset.movieDirectory = movieDir; %need to get external input from bash script?
+jobset.movieFiles =  kitFindFiles(movieDir, 'flowdec_deconvolved.ome.tif',1,0,1); %will find deconvolved movies
+%kitFindFiles(movieDir, '*_flowdec_deconvolved.ome.tif',1,0,1);
+
+for i=1:length(jobset.movieFiles)
+    [md,~]=kitOpenMovie(jobset.movieFiles{i});
+    crop = [1 1 md.frameSize(1:2)];
+    cropSize = md.frameSize(1:3);
+    r = length(jobset.ROI) + 1;
+    jobset.ROI(r).movie = jobset.movieFiles{i};
+    jobset.ROI(r).crop = crop;
+    jobset.ROI(r).cropSize = cropSize;
+end
+
+%compute avaerage search radius
+dt = 4.7;
+avgDisp = 3.6/60; %um/min -> um/s
+r = zeros(2,1);
+r(2) = 6*avgDisp*dt;
+r(1) = 0.1*avgDisp*dt;
+%set
+jobset.options.autoRadiidt = dt;
+jobset.options.autoRadiiAvgDisp = 3.6/60;
+jobset.options.minSearchRadius = [r(1) 3*r(1) r(1)];
+jobset.option.maxSearchRadius = [r(2) 3*r(2) r(2)];
+
+jobset.options.flatBackground = 1;
+
+jobset.filename = ['kitjobset_' datestr(now,'yymmdd') '_' identifier]
+
+
+end
+
+
