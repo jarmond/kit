18cb24ae3b7965b978aa8e9c5d218d335dece706
Jonathan Harrison <jonathan.u.harrison@warwick.ac.uk>
2020-08-26 12:16:29 +0100

    Added option to kitShowAllSpots to plot line profiles for each spot

--- kitShowAllSpots.m
+++ kitShowAllSpots.m
@@ -12,6 +12,7 @@ function kitShowAllSpots(job,varargin)
 %           contrast images. Tips:
 %               - Increase brightness by changing to [0.1 0.9]
 %               - Decrease background noise by changing to [0.5 1]
+%    lineProfile: {0} or 1. Whether to calculate and plot line profile of spot.
 %
 %    plotCoords: 0 or {1}. Whether or not to plot coordinates over images.
 %
@@ -27,6 +28,7 @@ function kitShowAllSpots(job,varargin)
 
 opts.channel = 1;
 opts.contrast = [0.1 1];
+opts.lineProfile = 0;
 opts.plotCoords = 1;
 opts.subset = [];
 opts.timePoint = 1;
@@ -76,6 +78,25 @@ for iSpot=opts.subset
         plotTit = sprintf('Spot %i',iSpot);
         title(plotTit,'FontSize',10)
     end
+end
+
+if opts.lineProfile
+    opts.scaleMax = max(img,[],'all')*8;    
+figure(2); clf
+for iSpot=opts.subset
+
+    opts.spotID = iSpot;
+
+    f = subplot(fig_m,fig_n,iSpot);
+
+    noCoords = plotLineProfile(job,img,opts);
+    if noCoords
+        cla(f);
+    else
+        plotTit = sprintf('Spot %i',iSpot);
+        title(plotTit,'FontSize',10)
+    end
+end
 end
 
 warning(w);
@@ -168,6 +189,87 @@ end
 hold on
 plot(coords(1),coords(2),...
     'Color','k','Marker','x','MarkerSize',10)
+hold off
+
+end
+
+function noCoords = plotLineProfile(job,img,opts)
+
+% get coordinate system and plot channels
+chan = opts.channel;
+refChan = job.options.coordSystemChannel;
+% get chromatic shift
+chrShift = job.options.chrShift.result{refChan,chan}(1:3);
+
+% get sister information
+tk = opts.spotID;
+tp = opts.timePoint;
+
+% get pixel resolution
+pixelSize = job.metadata.pixelSize;
+imageSize = size(img);
+
+if isfield(job.dataStruct{chan},'tracks')
+    % accumulate track information
+    track = job.dataStruct{chan}.tracks(tk);
+    startTime = track.seqOfEvents(1,1);
+    endTime   = track.seqOfEvents(2,1);
+    if tp < startTime || tp > endTime
+        coords = nan(1,3);
+    else
+        coords = ...
+            track.tracksCoordAmpCG(8*(tp-(startTime-1))-7:8*(tp-(startTime-1))-5);
+        coords = coords + chrShift;
+        coords = coords./pixelSize;
+    end
+else
+    % accumulate initCoord information
+    initCoord = job.dataStruct{chan}.initCoord(tp);
+    coords = initCoord.allCoordPix(tk,1:3);
+    chrShift = chrShift./pixelSize;
+    coords = coords + chrShift;
+end
+
+% check whether any coordinates have been found, plot nothing if so
+if any(isnan(coords))
+    noCoords = 1;
+    return
+else
+    noCoords = 0;
+end
+
+% calculate centre pixel
+centrePxl = round(coords);
+
+% max project over three z-slices around point
+if opts.zProject == 1
+    img = max(img, [], 3);
+elseif opts.zProject == -1
+    img = max(img(:,:,max(1,centrePxl(3)-2):min(centrePxl(3)+2,opts.imageSize(3))), [], 3);
+else
+    img = img(:,:,centrePxl(3));
+end
+
+% produce cropped image around track centre
+xReg = [centrePxl(1)-ceil(0.5/pixelSize(1))+1 ...
+           centrePxl(1)+ceil(0.5/pixelSize(1))+1];
+yReg = [centrePxl(2)-ceil(0.5/pixelSize(2))+1 ...
+           centrePxl(2)+ceil(0.5/pixelSize(2))+1];
+xReg(1) = max(xReg(1),1); xReg(2) = min(xReg(2),imageSize(2));
+yReg(1) = max(yReg(1),1); yReg(2) = min(yReg(2),imageSize(1));
+imgCrpd = img(yReg(1):yReg(2),xReg(1):xReg(2));
+
+% % define contrast stretch and apply
+% irange=stretchlim(imgCrpd,opts.contrast);
+% imgCrpd = imadjust(imgCrpd, irange, []);
+
+% correct coordinates to the cropped region
+coords(:,1:2) = coords(:,1:2) - [xReg(1) yReg(1)] + 1;
+
+plot((yReg(1):yReg(2)) - centrePxl(2), sum(imgCrpd,2),'c--','linewidth',2) %NB note dimensions 'switched' 
+ylim([0,opts.scaleMax]);
+hold on
+plot((xReg(1):xReg(2)) - centrePxl(1), sum(imgCrpd,1),'m--','linewidth',2)
 hold off
 
 end
